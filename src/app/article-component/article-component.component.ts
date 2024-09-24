60f435e49655cf8520c5985308669143f00662b3
Idrisse ANGAMA <idrisseangama2a@gmail.com>
2024-09-24 18:26:57 +0200

    finishing quest

--- src/app/article-component/article-component.component.ts
+++ src/app/article-component/article-component.component.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
+type Article = {
+  title: string,
+  author: string,
+  content: string,
+  image: string,
+  isPublished: boolean,
+  comment: string
+}
 
 @Component({
   selector: 'app-article-component',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './article-component.component.html',
   styleUrl: './article-component.component.scss'
 })
 export class ArticleComponentComponent {
-    article = {
-      title: 'Titre de l\'article',
-      author: 'John Doe',
-      content: 'Voici le contenu de l\'article.',
-      image: 'https://via.placeholder.com/350x150',
+    article : Article = {
+      title: 'Yoga, IdeaPad : Lenovo fait le plein de nouveaux PC',
+      author: 'Thomas Estimbre',
+      content: 'L’un des grands artisans de cette édition 2024 de l’IFA s’appelle Lenovo. Le géant chinois étoffe grandement sa gamme de PC portables, à commencer par ses machines grand public. L’intelligence artificielle est évidemment l’un des arguments des nouveaux ordinateurs portables, plusieurs étant d’ailleurs des PC Copilot+. ',
+      image: 'https://www.journaldugeek.com/app/uploads/2024/09/lenovo-pc-ifa-2024-6.jpg',
       isPublished: true,
       comment: ''
     };
