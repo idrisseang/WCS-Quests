5c98a159a8e73efe8218f6ad60d97ac091ca520c
Idrisse ANGAMA <idrisseangama2a@gmail.com>
2024-09-24 12:55:16 +0200

    Initializing project: adding tsconfig.json, creating class + properties + constructor

---
+++ encapsulation.ts
@@ -0,0 +1,9 @@
+class BankCustomer{
+    private name : string;
+    private secretCBCode: string;
+
+    constructor(name: string, secretCBCode: string){
+        this.name = name;
+        this.secretCBCode = secretCBCode;
+    }
+}
