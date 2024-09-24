af816c189eb2e099603fd7181bca0d77582a11ac
Idrisse ANGAMA <idrisseangama2a@gmail.com>
2024-09-24 13:01:39 +0200

    Modifying tests structure : add messages

--- encapsulation.ts
+++ encapsulation.ts
@@ -20,8 +20,14 @@ class BankCustomer{
 
 //Tests
 
-const customer = new BankCustomer('John Doe', '3579');
-assert.equal(typeof customer.getName, 'function');
-assert.equal(typeof customer.verifyPinInput, 'function');
-assert.equal(customer.getName(), 'John Doe');
-assert.ok(customer.verifyPinInput('3579'));
+try {
+    const customer = new BankCustomer('John Doe', '3579');
+    assert.equal(typeof customer.getName, 'function');
+    assert.equal(typeof customer.verifyPinInput, 'function');
+    assert.equal(customer.getName(), 'John Doe');
+    assert.ok(customer.verifyPinInput('3579'));
+
+    console.log("Tous les tests sont validés ✅");
+} catch (error) {
+    console.log("Au moins un des tests a échoué ! ", error);
+}
